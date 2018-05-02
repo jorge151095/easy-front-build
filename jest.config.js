cfe99ae58a0238bfd9a29e09d787d8c1d573fbdb
antonio.hernandez <jorgeantoniohernandezguzman@hotmail.com>
2018-05-02 14:54:49 -0500

    refactor(refactor conf)

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    verbose: true,
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'lib/**/*.js'
+    ],
+    coverageReporters: ['text-summary', 'html']
+};
